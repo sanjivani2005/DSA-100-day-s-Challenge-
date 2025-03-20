8c66eba8d115316fb63a2421ea04653918eb3f12
sanjivani2005 <137622808+sanjivani2005@users.noreply.github.com>
2025-03-20 19:46:28 +0530

    Update CalculateSimpleInterest .cpp

--- CalculateSimpleInterest .cpp
+++ CalculateSimpleInterest .cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+  double P,R,T, SI;
+  cout<< " Give the value of Principle , Rate and Time : "<<endl;
+  cin>>P>>R>>T;
+
+  SI= (P*R*T)/100;
+
+    cout<< "Simple Interest is : " << SI<< endl;
+
+    return 0;
+
+}
