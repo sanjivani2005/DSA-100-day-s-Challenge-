7ae291f71454a13a29dd602677a4a15cea25283e
sanjivani2005 <137622808+sanjivani2005@users.noreply.github.com>
2025-03-20 20:24:54 +0530

    Create sumOfN.cpp

---
+++ sumOfN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i=1,sum=0,N;
+
+    cout<<" Upto what no. you want to sum :";
+    cin>>N;
+
+    for(i=1;i<=N;i++){
+    if(i<=N){
+        sum=sum+i;
+    }
+}
+cout<<endl<<" Sum of first "<<N<<" Natural no. is : "<<sum<<endl;
+
+    return 0;
+}
