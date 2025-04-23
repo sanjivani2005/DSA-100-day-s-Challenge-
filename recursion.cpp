1a19706620376c33bbcdcfd56e02d12ce93d3cc0
sanjivani2005 <137622808+sanjivani2005@users.noreply.github.com>
2025-04-23 21:43:49 +0530

    Day 35

---
+++ recursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if (n <= 1)  // base case
+        return 1;
+    else
+        return n * factorial(n - 1);  // recursive call
+}
+
+int main() {
+    int num;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    if (num < 0)
+        cout << "Factorial is not defined for negative numbers." << endl;
+    else
+        cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
+    return 0;
+}
